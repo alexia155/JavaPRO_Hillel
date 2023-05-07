ccf41e20d7cfee0e935d0c68bbb10a5b0b215d48
Alexia <ola.prylutska@gmail.com>
2023-05-07 23:43:54 +0200

    Change min and max values

---
+++ src/hw3/Exercise11.java
@@ -0,0 +1,38 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        Scanner scannerNumbers = new Scanner(System.in);
+        System.out.println("Enter numbers to the array in one line: ");
+        String stringOfNumbers = scannerNumbers.nextLine();
+
+        String[] arrayOfString = stringOfNumbers.split(" ");
+        int min = Integer.parseInt(arrayOfString[0]);
+        int max = Integer.parseInt(arrayOfString[0]);
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < arrayOfString.length; i++) {
+            int parse = Integer.parseInt(arrayOfString[i]);
+            if (parse < min) {
+                min = parse;
+                minIndex = i;
+            }
+            if (parse > max) {
+                max = parse;
+                maxIndex = i;
+            }
+        }
+
+        arrayOfString[maxIndex] = Integer.toString(min);
+        arrayOfString[minIndex] = Integer.toString(max);
+
+        for(String s : arrayOfString) {
+            System.out.printf("%s ", s);
+        }
+
+
+    }
+}
