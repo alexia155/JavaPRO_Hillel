f025ed51767d488a2ad9fb5c975d040270eaf295
Alexia <ola.prylutska@gmail.com>
2023-05-07 19:31:03 +0200

    Check the factorial n! with the for loop

---
+++ src/hw3/Exercise2.java
@@ -0,0 +1,19 @@
+package hw3;
+
+public class Exercise2 {
+    static int n;
+    static int i;
+    static int factorial;
+
+    public static void main(String[] args) {
+        n = 10;
+        factorial = 1;
+
+        for (i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("n = " + n + ". Factorial of this number is " + factorial);
+
+    }
+}
