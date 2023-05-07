0a3a3d65991eb8593c67c430e332c2b3bf1fbda3
Alexia <ola.prylutska@gmail.com>
2023-05-07 20:10:50 +0200

    Calculate degree

---
+++ src/hw3/Exercise5.java
@@ -0,0 +1,19 @@
+package hw3;
+
+public class Exercise5 {
+    static int x;
+    static int n;
+    static int result;
+
+    public static void main(String[] args) {
+        x = 3;
+        n = 2;
+
+        System.out.println(calculateDegree(x, n));
+    }
+
+    private static int calculateDegree(int x, int n) {
+        result = (int) Math.pow(x, n);
+        return result;
+    }
+}
