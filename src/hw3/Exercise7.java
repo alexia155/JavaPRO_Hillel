4963f373101b5c08dfe807e6a9636074c2be46ad
Alexia <ola.prylutska@gmail.com>
2023-05-07 21:02:42 +0200

    Display the multiplication table

---
+++ src/hw3/Exercise7.java
@@ -0,0 +1,15 @@
+package hw3;
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+                System.out.printf("%3d * %3d = %3d \n", i, number, (i * number));
+            }
+        }
+    }
+
