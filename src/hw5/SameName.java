3051de7bb15fe253ee1319e9c6e229e66629873e
Alexia <ola.prylutska@gmail.com>
2023-05-17 12:01:33 +0200

    Creating two classes with the same name

---
+++ src/hw5/SameName.java
@@ -0,0 +1,8 @@
+package hw5;
+
+public class SameName {
+
+    public static void main(String[] args) {
+        System.out.println("This is the first class with the name 'Same Name', which is located in the main homework package.");
+    }
+}
---
+++ src/hw5/task/SameName.java
@@ -0,0 +1,7 @@
+package hw5.task;
+
+public class SameName {
+    public static void main(String[] args) {
+        System.out.println("This is the second class with the same name 'Same Name', which is located in the new separate package.");
+    }
+}
